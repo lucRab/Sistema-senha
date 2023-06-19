d19bd55b4093b8c2bebc81169856ad72d13bb472
guilherme <guilhermekeslley123@gmail.com>
2023-06-19 13:59:49 -0300

    Função para passar querys e executa-las

---
+++ core/validations/validConnection.class.php
@@ -0,0 +1,27 @@
+<?php
+class validConnection
+{
+    public function __construct()
+    {
+    }
+    /**
+        Essa função serve para substituir os try/catch toda vez
+        que precisar executar uma query no banco, evita repetições e fica mais legível
+        Exemplo na classe: classesModel.class.php
+     */
+    public static function isValidConnection($conexao, $query, $params)
+    {
+        try {
+            if (gettype($conexao)!== 'string' and get_class($conexao) === 'PDO') {
+                $consulta = $conexao->prepare($query);
+                $consulta->execute($params);
+                return $consulta;
+            } else {
+                throw new \PDOException($conexao);
+            }
+        } catch(PDOException $e) {
+            return 'ERROR: ' . $e->getMessage();
+        }
+    }
+
+}
