ea84e5b23037fd1854f5155d2aef0ba87f86e75a
guilherme <guilhermekeslley123@gmail.com>
2023-06-19 13:58:54 -0300

    Cursos

--- app/model/coursesModel.class.php
+++ app/model/coursesModel.class.php
@@ -1,13 +1,25 @@
 <?php
 
-  class CoursesModel {
+namespace app\model;
 
-    public function __construct(){}
-
-
-    public static function getAllCourses($conexao) {
-      $select = $conexao->prepare("SELECT * FROM curso");
-      if ($select->execute()) return true;  
+class CoursesModel
+{
+    public function __construct()
+    {
     }
 
-  }
+
+    public static function getAllCourses($conexao)
+    {
+        $select = $conexao->prepare("SELECT * FROM curso");
+        if ($select->execute()) {
+            return true;
+        }
+    }
+
+    public static function getDataCourse($course)
+    {
+        $classes = new \app\controller\ClassesController($course);
+    }
+
+}
