51cb8f8b8d94856fae63b726ab71618c114b7c1a
lucRab <99261962+lucRab@users.noreply.github.com>
2023-06-21 21:27:38 -0300

    Testes das senhas

--- testes/testePassword.php
+++ testes/testePassword.php
@@ -10,47 +10,42 @@ require_once "../app/controller/classesController.class.php";
 require_once "../database/sqlClasses.php";
 $conexao = Conexao::conectar();
 
+//// PARAM
 
-/// SELECT
-// try {
-//     if (gettype($conexao) !== 'string' and get_class($conexao) == 'PDO') {
-//       $data = $conexao->query('SELECT * FROM solicitacao');
-//       while($row = $data->fetch(PDO::FETCH_OBJ)) {
-//         print_r($row);
-//       }
-//     } else throw new PDOException($conexao);
-//   } catch(PDOException $e) {
-//       echo 'ERROR: ' . $e->getMessage();
-//   }
+// $pass = new stdClass();
+
+// $pass->cod_aluno = null;
+// $pass->cod_senha = 3;
 
 
-//// USER
+// $teste = new PasswordModel;
+// $teste->alterarPassword($conexao,$pass);
+// var_dump($teste);
+//-------------------------------------------------------------------\\
 
-$pass = new stdClass();
+//// PARAM
 
-$pass->cod_aluno = null;
-$pass->cod_senha = 3;
+//  $param = new stdClass();
 
-// var_dump(UserModel::updateUser($conexao, $user));
-// die();
+// $param->cod_aluno = 1;
 
-//// VALIDAÇÕES
+// $teste = new PasswordModel;
+//  $result = $teste->userPassword($conexao,$param);
 
-// $email = 'carlosgmail.com';
-// $testeEmail = ValidData::isValidEmail($email);
+// while($row = $result->fetch(PDO::FETCH_OBJ)) {
+//          print_r($row);
+// }
+//-----------------------------------------------------------------\\
 
-// $cpf = '904.554.270-62';
-// $testeEmail = ValidData::isValidCPF($cpf);
+//PARAM
 
-// $password = 'Teste999!a';
-// $testePassword = ValidData::isValidPassword($password);
+$param = new stdClass();
 
-//// CLASSES
-
-//$courses = \app\model\CoursesModel::getAllCourses($conexao);
-//$coe = \app\controller\ClassesController::filterByAge(10);
+$param->cod_turma = 2819;
 
 $teste = new PasswordModel;
-$teste->alterarPassword($conexao,$pass);
+$result = $teste->passwordOpen($conexao,$param);
 
-var_dump($teste);
+while($row = $result->fetch(PDO::FETCH_OBJ)) {
+  print_r($row);
+}
