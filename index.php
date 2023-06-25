e436521b6c383c3061c0d25c6cb8ba9589581fcd
guilherme <guilhermekeslley123@gmail.com>
2023-06-24 22:50:47 -0300

    Rotas de post/get

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
 
 require_once __DIR__ . "/vendor/autoload.php";
-use App\controller\ClassesController;
 use App\controller\UserController;
+use App\controller\CoursesController;
+use App\controller\ClassesController;
 use CoffeeCode\Router\Router;
-
 
 $router = new Router("http://localhost/Sistema-Senha");
 
@@ -12,6 +12,21 @@ $router = new Router("http://localhost/Sistema-Senha");
   Controllers
 */
 $router->namespace("App\controller");
+
+/* 
+  Home
+*/
+$router->group('');
+$router->get("/", "CoursesController:getAllCourses");
+
+/* 
+  Cursos
+*/
+$router->group('curso');
+$router->get("/{course}", "CoursesController:getCourse");
+
+$router->post("/{course}", "ClassesController:filterClassesController");
+$router->post("/{course}/{shift}", "ClassesController:filterClassesController");
 
 /* 
   Registro / Login
@@ -23,7 +38,6 @@ $router->post("/", "UserController:createUser");
 /* 
   Error
 */
-
 $router->group('ooops');
 $router->get("/{errcode}", "UserController:error");
 
